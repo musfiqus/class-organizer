4d8a5e0dcfacbaf4217cb9392ceabaad2be41851
musfiqus <musfiqus@gmail.com>
2018-06-01 04:04:39 +0600

    4.7: Viewholder is now dependent on user type

--- app/src/main/java/bd/edu/daffodilvarsity/classorganizer/fragment/DayFragment.java
+++ app/src/main/java/bd/edu/daffodilvarsity/classorganizer/fragment/DayFragment.java
@@ -42,7 +42,9 @@ public class DayFragment extends Fragment implements DayDataAdapter.DayListItemC
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         assert courseData != null;
-        adapter = new DayDataAdapter(courseData, getContext(), R.layout.list_item, DayDataAdapter.HOLDER_ROUTINE, this);
+        adapter = new DayDataAdapter(courseData, getContext(), R.layout.list_item,
+                prefManager.isUserStudent() ? DayDataAdapter.HOLDER_ROUTINE: DayDataAdapter.HOLDER_TEACHER,
+                this);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
         return rootView;
